01f23d00c6a46ee148553a39a2aef9ea444e8d20
Will Thirkettle <hello@willthirkettle.co.uk>
2016-04-29 13:54:20 +0100

    Add screenUtils - wip

--- app/util/screen.js
+++ app/util/screen.js
@@ -1,8 +1,19 @@
 
 const MAX_SIZE = 1024;
+const PIXEL_RATIO = window.devicePixelRatio || 1;
 // const MAX_WIDTH = 1024;
 // const MAX_HEIGHT = 768;
 const isTouchEnabled = 'createTouch' in window.document;
+
+const getWidth = () => {
+	return window.document.body.clientWidth;
+	return Math.min( window.screen.width * PIXEL_RATIO, MAX_SIZE );
+}
+
+const getHeight = () => {
+	return window.document.body.clientHeight;
+	return Math.min( window.screen.height * PIXEL_RATIO, MAX_SIZE );
+}
 
 const screenUtils = {
 
@@ -10,8 +21,10 @@ const screenUtils = {
 
   getDimensions: () => {
     return {
-      width: Math.min( window.document.body.clientWidth, MAX_SIZE ),
-      height: Math.min( window.document.body.clientHeight, MAX_SIZE )
+      // width: screen.width,
+      // height: screen.height
+      width: getWidth(),
+      height: getHeight()
     }
   }
 
